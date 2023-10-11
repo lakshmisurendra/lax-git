9bb456e938075b40d5ac4f869b5ae8a975de4a22
lakshmisurendra <72179060+lakshmisurendra@users.noreply.github.com>
2023-10-10 21:30:06 -0400

    non static

--- Var.java
+++ Var.java
@@ -1,5 +1,5 @@
 public class Var {
     public static void main(String[] args) {
-        System.out.println("Instance variables");
+        System.out.println("Instance variables or non static fields");
     }
 }
